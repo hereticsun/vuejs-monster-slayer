d4a9de2402c8eb04296841ddbc3992221d0c20dd
Azlan Cuttilan <azlan@bynd.com>
2017-01-16 12:13:31 +0000

    Start, quit & monster turn methods
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- js/app.js
+++ js/app.js
@@ -3,19 +3,34 @@ new Vue ({
     data: {
         play: false,
         actions: [
-            {
-                class: 'monster-turn',
-                desc: 'Monster hits player'
-            },
-            {
-                class: 'player-turn',
-                desc: 'Player hits monster'
-            }
         ],
         yourHealth: 100,
         monsterHealth: 100
     },
     computed: {},
-    methods: {},
+    methods: {
+        startGame: function() {
+            this.play = true;
+            this.actions = [];
+            this.yourHealth = 100;
+            this.monsterHealth = 100;
+        },
+        quitGame: function() {
+            this.play = false;
+        },
+        monsterTurn: function() {
+            var dmg = Math.floor(Math.random() * 10) + 1;
+            this.yourHealth -= dmg;
+            var action = {
+                class: 'monster-turn',
+                desc: 'Monster hits player for ' + dmg
+            };
+/*
+            action.class = 'monster-turn';
+            action.desc = 'Monster hits player for ' + dmg;
+*/
+            this.actions.push(action);
+        }
+    },
     watch: {}
 });
