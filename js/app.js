c59ff14b662eee8b08cbc67df0835f5cee6fbc33
Azlan Cuttilan <azlan@bynd.com>
2017-01-16 11:19:47 +0000

    Turn actions
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ new Vue ({
     el: '#app',
     data: {
         play: false,
+        actions: [
+            {
+                class: 'monster-turn',
+                desc: 'Monster hits player'
+            },
+            {
+                class: 'player-turn',
+                desc: 'Player hits monster'
+            }
+        ],
         yourHealth: 80,
         monsterHealth: 80
     },
